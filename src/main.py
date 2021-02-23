42157c4260d642dc66fe63a46f6a7337a6f73d8a
ampasancho <amparo.sancho.arellano@alumnos.upm.es>
2021-02-23 17:40:04 +0100

    Archivo main con la cuenta 0 to 10 incluida

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 def print_hi(name):
     print("Hola, {0}".format(name))
 
+
+def print_i (i):
+    print("El numero es: {0}".format(i))
+
+
 if __name__ == '__main__':
     print_hi('a todos')
+    for i in range(11):
+        print_i (i)
+
 
 
