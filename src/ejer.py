2ff5dd4d7e9e727632899d98cf856e85e134936d
JuanSegura98 <juanseguranunez@gmail.com>
2021-02-24 17:47:21 +0100

    Creado ejer.py

---
+++ src/ejer.py
@@ -0,0 +1,11 @@
+def showNumbers:
+    number = input("Introduce numero: ")
+    for i in range(0, number+1)
+        print(i + "\n")
+    
+def isOdd:
+    number = input("Introduce numero: ")
+    if number%2:
+        print("Es impar")
+    else:
+        print("Es par")
